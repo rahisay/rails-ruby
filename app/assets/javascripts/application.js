ececcc5e56e46618b1e190c240bbf8c639beff68
raheemgul <rahisay@yahoo.com>
2018-03-22 11:06:46 +0300

    reorder javascirpt

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
-//= require popper
+//= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
